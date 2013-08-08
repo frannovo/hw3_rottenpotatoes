320900e411156e0672888dafc4d96c3cce8330f7
frannovo <fjnovo@gmail.com>
2013-08-08 20:06:23 +0200

    Hw done

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie) if movie[:title] != 'title'
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,21 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  content = page.body
+  assert content.index(e1) < content.index(e2)
+end
+
+Then /I should see all of the movies/ do
+  movies_count = Movie.count
+  assert_equal movies_count, page.all("#movies tbody tr").size 
+  #Movie.find(:all).each do |movie|
+  #  title = movie["title"]
+  #  if page.respond_to? :should
+  #    page.should have_content(title)
+  #  else
+  #    assert page.has_content?(title)
+  #  end
+  #end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +40,8 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/\,\s?/).each do |rating|
+    step %Q{I #{uncheck}check "ratings_#{rating}"}
+  end
 end
+
